1bd4ad52fd5cb8ed8b0e5056234a76885e9d33af
Haocheng Tang <120698463@qq.com>
2020-09-20 21:43:03 +0800

    init

---
+++ common/common-utils/src/main/java/com/thc/commonutils/ResultCode.java
@@ -0,0 +1,16 @@
+package com.thc.commonutils;
+
+/**
+ * @author thc
+ * @Title:
+ * @Package com.thc.commonutils
+ * @Description:
+ * @date 2020/8/12 3:05 下午
+ */
+public interface ResultCode {
+
+
+    Integer SUCCESS = 20000; //成功
+
+    Integer ERROR = 20001; //失败
+}
