e5a16223fdeaf08ca34cfbca5015de1248312731
Haocheng Tang <120698463@qq.com>
2020-09-20 22:20:34 +0800

    jwt init fix
    jwt init fix

---
+++ service/server-jwt/src/main/java/com/thc/serverjwt/entity/TestEntity.java
@@ -0,0 +1,14 @@
+package com.thc.serverjwt.entity;
+
+import lombok.Data;
+
+/**
+ * @author thc
+ * @Title:
+ * @Package com.thc.serverjwt.entity
+ * @Description: test
+ * @date 2020/9/20 10:19 下午
+ */
+@Data
+public class TestEntity {
+}
